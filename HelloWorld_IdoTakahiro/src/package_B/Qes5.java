bb2bdd8b3e43559e599d97e59e8efab43303742e
ABC <ABC@192.168.10.108>
2025-07-16 15:22:25 +0900

    修正版

--- HelloWorld_IdoTakahiro/src/package_B/Qes5.java
+++ HelloWorld_IdoTakahiro/src/package_B/Qes5.java
@@ -2,41 +2,41 @@ package package_B;
 
 public class Qes5 {
 	public static void main(String[] args) {
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 1)) + "="
-				+ (String.format("%03d", i * 1)) + "||"+"\n");
+				+ (String.format("%03d", i * 1)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 2)) + "="
-				+ (String.format("%03d", i * 2)) + "||"+"\n");
+				+ (String.format("%03d", i * 2)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 3)) + "="
-				+ (String.format("%03d", i * 3)) + "||"+"\n");
+				+ (String.format("%03d", i * 3)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 4)) + "="
-				+ (String.format("%03d", i * 4)) + "||"+"\n");
+				+ (String.format("%03d", i * 4)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 5)) + "="
-				+ (String.format("%03d", i * 5)) + "||"+"\n");
+				+ (String.format("%03d", i * 5)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 6)) + "="
-				+ (String.format("%03d", i * 6)) + "||"+"\n");
+				+ (String.format("%03d", i * 6)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 7)) + "="
-				+ (String.format("%03d", i * 7)) + "||"+"\n");
+				+ (String.format("%03d", i * 7)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i < 21; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 8)) + "="
-				+ (String.format("%03d", i * 8)) + "||"+"\n");
+				+ (String.format("%03d", i * 8)) + "||");
 	}
-	for (int i = 1; i < 10; i++) {
+	for (int i = 1; i <21 ; i++) {
 		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 9)) + "="
-				+ (String.format("%03d", i * 9)) + "||"+"\n");
+				+ (String.format("%03d", i * 9)) + "||");
 	}
 }
 }
