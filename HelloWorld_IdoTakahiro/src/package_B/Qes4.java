3a1d29e89baa41c1aca991f45f6b0574bf710cfd
ABC <ABC@192.168.10.108>
2025-08-09 01:18:06 +0900

    修正版

--- HelloWorld_IdoTakahiro/src/package_B/Qes4.java
+++ HelloWorld_IdoTakahiro/src/package_B/Qes4.java
@@ -3,25 +3,16 @@ package package_B;
 public class Qes4 {
 	public static void main(String[] args) {
 		for (int i = 1; i < 10; i++) {
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 1)) + "="
-					+ (String.format("%02d", i * 1)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 2)) + "="
-					+ (String.format("%02d", i * 2)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 3)) + "="
-					+ (String.format("%02d", i * 3)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 4)) + "="
-					+ (String.format("%02d", i * 4)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 5)) + "="
-					+ (String.format("%02d", i * 5)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 6)) + "="
-					+ (String.format("%02d", i * 6)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 7)) + "="
-					+ (String.format("%02d", i * 7)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 8)) + "="
-					+ (String.format("%02d", i * 8)) + "||");
-			System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", 9)) + "="
-					+ (String.format("%02d", i * 9))+"\n");
-			
-		}}}
-		
+			for (int j = 1; j < 10; j++) {
+				if (j< 9) {
+					System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", j)) + "="
+							+ (String.format("%02d", i * j)) + "||");
+				} else if (j > 8) {
+					System.out.print((String.format("%02d", i)) + "*" + (String.format("%02d", j)) + "="
+							+ (String.format("%02d", i * j)) + "\n");
 
+				}
+			}
+		}
+	}
+}
--- HelloWorld_IdoTakahiro/src/package_B/Qes5.java
+++ HelloWorld_IdoTakahiro/src/package_B/Qes5.java
@@ -3,40 +3,19 @@ package package_B;
 public class Qes5 {
 	public static void main(String[] args) {
 	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 1)) + "="
-				+ (String.format("%03d", i * 1)) + "||");
+		
+		for(int j = 1; j < 21; j++) {
+			if( j < 20) {
+		System.out.print((String.format("%03d", j)) + "*" + (String.format("%03d", i)) + "="
+				+ (String.format("%03d", j * i)) + "||");
+	
+		}else if( j > 19) {
+				System.out.print((String.format("%03d", j)) + "*" + (String.format("%03d", i)) + "="
+						+ (String.format("%03d", j * i)) + "||"+"\n");
+				}
+		
 	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 2)) + "="
-				+ (String.format("%03d", i * 2)) + "||");
 	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 3)) + "="
-				+ (String.format("%03d", i * 3)) + "||");
-	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 4)) + "="
-				+ (String.format("%03d", i * 4)) + "||");
-	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 5)) + "="
-				+ (String.format("%03d", i * 5)) + "||");
-	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 6)) + "="
-				+ (String.format("%03d", i * 6)) + "||");
-	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 7)) + "="
-				+ (String.format("%03d", i * 7)) + "||");
-	}
-	for (int i = 1; i < 21; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 8)) + "="
-				+ (String.format("%03d", i * 8)) + "||");
-	}
-	for (int i = 1; i <21 ; i++) {
-		System.out.print((String.format("%03d", i)) + "*" + (String.format("%03d", 9)) + "="
-				+ (String.format("%03d", i * 9)) + "||");
 	}
 }
-}
+
