0dba2a48b9bffca2e2d868557df6a3541629be28
ABC <ABC@192.168.10.107>
2025-09-11 21:15:50 +0900

    1_22完了いたしましたので確認お願いいたします。

---
+++ HelloWorld_IdoTakahiro/src/package1_22_2/sub.java
@@ -0,0 +1,19 @@
+package package1_22_2;
+
+public class sub {
+	public String greeting;
+	public String sushi1;
+	public String sushi2;
+	
+	public sub(){
+		this.greeting = "こんにちは！ここは日本です！";
+		this.sushi1 = "この寿司はうまい";
+		this.sushi2 = "寿司は和食です";
+	}
+	
+	
+
+	
+		
+
+	}
