e57521f91edc72cf6717c570be07af51212c3af3
ABC <ABC@192.168.10.107>
2025-09-16 17:27:02 +0900

    修正いたしました。ご確認お願いいたします。

--- HelloWorld_IdoTakahiro/src/package1_22/main.java
+++ HelloWorld_IdoTakahiro/src/package1_22/main.java
@@ -8,33 +8,30 @@ import package1_22_2.sub;
 public class main {
 
 	public static void main(String[] args) {
-//		下記がコンソールに出力されるように作成してください
-//		
-//		こんにちは！ここは日本です！
-//		この寿司はうまい
-//		寿司は和食です
-		
+		//		下記がコンソールに出力されるように作成してください
+		//		
+		//		こんにちは！ここは日本です！
+		//		この寿司はうまい
+		//		寿司は和食です
+
 		sub sub = new sub();
 		System.out.println(sub.greeting);
 		System.out.println(sub.sushi1);
 		System.out.println(sub.sushi2);
-		
-//		今の現在日時は2023/03/09 10:23:39です
+
+		//		今の現在日時は2023/03/09 10:23:39です
 		LocalDateTime nowDate = LocalDateTime.now();
-//		System.out.println(nowDate);
 		// 表示形式を指定
-				DateTimeFormatter dtf1 =
-					DateTimeFormatter.ofPattern("yyyy/MM/dd H:mm:ss"); // ①表示形式を設定
-				String formatNowDate = dtf1.format(nowDate); // ②nowDate(現在時刻)をdtf1に入れる
-				System.out.println("今の現在日時は"+formatNowDate+"です"); 
-	
-		
-//
-//	【条件】
-//		・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
-//		・適切なファイルにフィールドで変数を必要な数作ってください。
-//		・thisを使って作成してください。
-//		・日時は今日の日付を取得してください。
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy/MM/dd H:mm:ss"); // ①表示形式を設定
+		String formatNowDate = dtf1.format(nowDate); // ②nowDate(現在時刻)をdtf1に入れる
+		System.out.println("今の現在日時は" + formatNowDate + "です");
+
+		//
+		//	【条件】
+		//		・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+		//		・適切なファイルにフィールドで変数を必要な数作ってください。
+		//		・thisを使って作成してください。
+		//		・日時は今日の日付を取得してください。
 
 	}
 
--- HelloWorld_IdoTakahiro/src/package1_22_2/sub.java
+++ HelloWorld_IdoTakahiro/src/package1_22_2/sub.java
@@ -4,16 +4,11 @@ public class sub {
 	public String greeting;
 	public String sushi1;
 	public String sushi2;
-	
-	public sub(){
+
+	public sub() {
 		this.greeting = "こんにちは！ここは日本です！";
 		this.sushi1 = "この寿司はうまい";
 		this.sushi2 = "寿司は和食です";
 	}
-	
-	
 
-	
-		
-
-	}
+}
