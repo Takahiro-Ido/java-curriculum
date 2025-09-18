f5e7e9b2631c13ed7b9c669de55006b47621ed5f
ABC <ABC@192.168.10.107>
2025-09-18 20:22:02 +0900

    課題完了いたしましたので、ご確認お願いいたします。

---
+++ HelloWorld_IdoTakahiro/src/package1_23/Main.java
@@ -0,0 +1,19 @@
+package package1_23;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			Sub sub = new Sub();
+			 sub.setName("ライオン");
+			 sub.setHeight(2.1);
+			 sub.setSpeed(80);
+			 
+			System.out.println("動物名："+sub.getName());
+			System.out.println("体長："+sub.getHeight()+"m");
+			System.out.println("速度："+sub.getSpeed()+"km/h");
+		}
+
+	}
+
---
+++ HelloWorld_IdoTakahiro/src/package1_23/Sub.java
@@ -0,0 +1,44 @@
+package package1_23;
+
+public class Sub {
+	//	下記がコンソールに出力されるように作成してください
+	// 	※thisとsetterとgetterとフィールドを使ってください
+	// 	
+	//  	動物名：ライオン
+	//	体長：2.1m
+	//	速度：80km/h
+	private String name;
+	private double height;
+	private int speed;
+
+	public String getName() {
+		return name;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setName(String name) { 
+		this.name = name; 
+	}
+	
+	public void setHeight(double height) { 
+		this.height = height; 
+	}
+	
+	public void setSpeed(int speed) { 
+		this.speed = speed; 
+	}
+
+	//	public Qes_1_23() {
+	//		this.name = "ライオン";
+	//		this.height = "2.1m";
+	//		this.speed = "80km/h";
+	//	}
+
+}
